128e9f1b7048ac020605813d2b8c056bd3021093
ntiKentucky <57523317+ntiKentucky@users.noreply.github.com>
2020-03-19 11:58:29 +0100

    random gender

--- js/script.js
+++ js/script.js
@@ -1,27 +1,7 @@
-var myArray = ['Male', 'Female'];
+document.querySelector('.genGender').addEventListener('click', gentext);
 
-document.getElementById("genderbox").addEventListener("click", displaydate);
-
-const randomElement = myArray[Math.floor(Math.random())]
-
-// var NumberOfWords = 2;
-
-var words = new BuildArray(NumberOfWords)
-
-words[1] = "Female"
-words[2] = "Male"
-
-
-function BuildArray(size){
-this.length = size
-for (var i = 1; i <= size; i++){
-this[i] = null}
-return this
+function gentext() {
+    var word=['Female','Male'];
+    var para=document.querySelector('p');
+    para.innerHTML=word[Math.floor(Math.random()*word.length)];
 }
-
-function PickRandomWord() {
-var rnd = Math.ceil(Math.random() * NumberOfWords)
-
-genderbox.value = words[rnd];
-}
-PickRandomWord();
