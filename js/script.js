0cca7e777bc3e2ced1ff9e7d062c81a608862626
ntiKentucky <57523317+ntiKentucky@users.noreply.github.com>
2020-03-19 12:48:22 +0100

    latest

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 document.querySelector('.genGender').addEventListener('click', gentext);
 
 function gentext() {
-    var word=['Female','Male'];
+    var word=['FEMALE','MALE'];
+    var para=document.querySelector('p');
+    para.innerHTML=word[Math.floor(Math.random()*word.length)];
+}
+
+document.querySelector('.randomHeight').addEventListener('click', Height);
+
+function Height() {
+    var word=['FEMALE','MALE'];
     var para=document.querySelector('p');
     para.innerHTML=word[Math.floor(Math.random()*word.length)];
 }
